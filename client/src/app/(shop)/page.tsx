3a01063ba8020300bc8a5081accb3f29e4c352df
Serhii Propolov <job.tenshi@gmail.com>
2025-08-27 15:31:52 +0300

    seo update (not ready yet)

--- client/src/app/(shop)/page.tsx
+++ client/src/app/(shop)/page.tsx
@@ -3,6 +3,21 @@ import { ICollection } from "@/features/collections/types/collections.types";
 import { Welcome } from "@/shared/components";
 import ShopTitle from "@/shared/ui/titles/ShopTitle";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Mindset – головна сторінка",
+    description: "Купуйте товари, які асоціюються з вашоб особистістю.",
+    openGraph: {
+        title: "Mindset",
+        description: "Інтернет магазин особливого одягу.",
+        url: "https://localhost:3000",
+        siteName: "Mindset",
+        locale: "uk_UA",
+        type: "website",
+    },
+};
+
 export default async function CollectionsPage() {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/shop/collections`,
---
+++ client/src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "http://localhost:3000",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1,
+        },
+        {
+            url: "http://localhost:3000/collections",
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 0.8,
+        },
+    ];
+}
