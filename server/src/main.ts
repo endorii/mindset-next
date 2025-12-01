cc646a2bbefb6304640444a4e364d25ad31a4e8b
Serhii Propolov <job.tenshi@gmail.com>
2025-12-01 22:31:05 +0100

    cors test

--- server/src/main.ts
+++ server/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     const frontendUrl = process.env.FRONTEND_URL || "http://localhost:3000";
 
     app.enableCors({
-        origin: frontendUrl,
+        origin: [frontendUrl, "https://mindset-client-production.up.railway.app"],
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
     });
