f04d042101c9f02e51de63cd52c3b05280a24b91
Serhii Propolov <job.tenshi@gmail.com>
2025-12-02 11:19:54 +0100

    try to fix SMTP connection

--- server/src/email/email.module.ts
+++ server/src/email/email.module.ts
@@ -1,19 +1,19 @@
-import { Module } from "@nestjs/common";
-import { EmailService } from "./email.service";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MailerModule } from "@nestjs-modules/mailer";
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { EmailService } from "./email.service";
 
 @Module({
     imports: [
         ConfigModule,
         MailerModule.forRootAsync({
-            imports: [ConfigModule], // потрібно для ConfigService
+            imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (config: ConfigService) => ({
                 transport: {
                     host: config.get<string>("MAIL_HOST"),
-                    port: config.get<number>("MAIL_PORT"),
-                    secure: config.get<boolean>("MAIL_SECURE"), // true для 465, false для 587
+                    port: Number(config.get<string>("MAIL_PORT")),
+                    secure: config.get<string>("MAIL_SECURE") === "true",
                     auth: {
                         user: config.get<string>("MAIL_USER"),
                         pass: config.get<string>("MAIL_PASS"),
